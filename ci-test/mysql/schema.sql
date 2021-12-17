025d0106b8c6eba0df96650f0e117140824a8dac
nursahb <nursah.bayat@ciceksepeti.com>
2021-12-17 20:19:44 +0300

    data added

--- ci-test/mysql/schema.sql
+++ ci-test/mysql/schema.sql
@@ -1,9 +1,17 @@
 create table Users
 (
-    UserId int auto_increment
-        primary key,
+    UserId int primary key,
     Name   varchar(100) charset utf8 not null,
     Age    tinyint                   not null
 );
 
-INSERT INTO UserDB.Users (UserId, Name, Age) VALUES (1, 'John Doe', 30);
+INSERT INTO Users (UserId, Name, Age) VALUES (1, 'John Doe', 32);
+INSERT INTO Users (UserId, Name, Age) VALUES (2, 'Jane Doe', 25);
+INSERT INTO Users (UserId, Name, Age) VALUES (3, 'Janie Doe', 12);
+INSERT INTO Users (UserId, Name, Age) VALUES (4, 'Janie Doe', 32);
+INSERT INTO Users (UserId, Name, Age) VALUES (5, 'Danny James', 45);
+
+INSERT INTO Users (UserId, Name, Age) VALUES (6, 'Rick Sanchez', 64);
+INSERT INTO Users (UserId, Name, Age) VALUES (7, 'Morty Smith', 13);
+
+INSERT INTO Users (UserId, Name, Age) VALUES (8, 'Jake Peralta', 28);
