504bb351ca9a93030e8482f3090e977090c66f47
nursahb <nursah.bayat@ciceksepeti.com>
2021-12-28 12:32:32 +0300

    load test data is added + imposters and test data are updated

--- ci-test/mysql/schema.sql
+++ ci-test/mysql/schema.sql
@@ -10,8 +10,14 @@ INSERT INTO Users (UserId, Name, Age) VALUES (2, 'Jane Doe', 25);
 INSERT INTO Users (UserId, Name, Age) VALUES (3, 'Janie Doe', 12);
 INSERT INTO Users (UserId, Name, Age) VALUES (4, 'Janie Doe', 32);
 INSERT INTO Users (UserId, Name, Age) VALUES (5, 'Danny James', 45);
-
 INSERT INTO Users (UserId, Name, Age) VALUES (6, 'Rick Sanchez', 64);
 INSERT INTO Users (UserId, Name, Age) VALUES (7, 'Morty Smith', 13);
-
 INSERT INTO Users (UserId, Name, Age) VALUES (8, 'Jake Peralta', 28);
+INSERT INTO Users (UserId, Name, Age) VALUES (9, 'Sheldon Cooper', 34);
+INSERT INTO Users (UserId, Name, Age) VALUES (10, 'Eleanor Shellstrop', 35);
+INSERT INTO Users (UserId, Name, Age) VALUES (11, 'Leonard Hotstadter', 21);
+INSERT INTO Users (UserId, Name, Age) VALUES (12, 'Stuart Bloom', 47);
+INSERT INTO Users (UserId, Name, Age) VALUES (13, 'Charles Boyle', 29);
+INSERT INTO Users (UserId, Name, Age) VALUES (14, 'Ray Holt', 38);
+INSERT INTO Users (UserId, Name, Age) VALUES (15, 'Luke Skywalker', 29);
+INSERT INTO Users (UserId, Name, Age) VALUES (16, 'Jesse Pinkman', 16);
